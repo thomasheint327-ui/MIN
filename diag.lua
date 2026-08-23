bb7e7b36942cdcaaf0078ce0c812397c4c5ddfde
thomasheint327-ui <thomasheint327@gmail.com>
2026-08-24 00:27:26 +0200

    diag contraption dump

--- diag.lua
+++ diag.lua
@@ -1,4 +1,3 @@
--- Cherche et ouvre le modem automatiquement
 local modem = peripheral.find("modem")
 if modem then
     rednet.open(peripheral.getName(modem))
@@ -6,29 +5,24 @@ else
     error("Aucun modem trouve sur ce vehicule !")
 end
 
-local monNom = "MISSILE-1" -- Change le nom selon le vaisseau
+local monNom = "MISSILE-1" -- Modifie ce nom selon ton engin
 
-print("Demarrage de la telemetrie...")
+print("Telemetrie HAUTE FREQUENCE (Max Hz) activee...")
 
 while true do
-    -- Le vaisseau demande sa position aux 4 satellites (timeout de 2 secondes)
-    local x, y, z = gps.locate(2)
+    -- On demande la position
+    local x, y, z = gps.locate()
     
     if x then
-        -- Si le GPS marche, on prépare un paquet de données
-        local donnees = {
+        -- On envoie les données
+        rednet.broadcast({
             id = monNom,
             posX = math.floor(x),
             posY = math.floor(y),
             posZ = math.floor(z)
-        }
-        
-        -- On envoie les données sur le protocole "AEROSPATIAL"
-        rednet.broadcast(donnees, "AEROSPATIAL")
-        print("Position envoyee : X:"..donnees.posX.." Y:"..donnees.posY.." Z:"..donnees.posZ)
-    else
-        print("Erreur : Signal GPS perdu !")
+        }, "AEROSPATIAL")
     end
     
-    sleep(0.2) -- Mise à jour très rapide (5 fois par seconde)
+    -- 0.05 seconde = 1 tick. C'est la limite physique de Minecraft.
+    sleep(0.05)
 end
